93e16b0ecbc49ed61a4180daa50d9e52d9bb93e3
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-03-21 01:09:54 +0300

    Домашнее задание 2-8. Коллекции. Многообразие реализаций - начальный коммит

---
+++ src/main/java/pro/sky/homework_2_8/JavaSkyProHomework28Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProHomework28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProHomework28Application.class, args);
+	}
+
+}
